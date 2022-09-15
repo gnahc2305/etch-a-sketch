3705ba3a37665e9e3d527f2869a872187cd94283
gnahc2305 <achangmurillo@gmail.com>
2022-09-15 14:01:25 -0500

    Entering a value now changes the amount of squares

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
 const mainContainer_div = document.querySelector('.mainContainer');
-let sliderValue = 16 // document.getElementById('slider').value;
-
-
-function calculateSize(sliderValue) {
-    const mainContainerHeight = 600;
-    const mainContainerWidth = 600;
-    heightOfDiv = (mainContainerHeight / sliderValue) - 2;
-    widthOfDiv = (mainContainerWidth / sliderValue) - 2;
-}
-
-calculateSize(sliderValue);
 
 function createDivs(numberOfDivs) {
-    numberOfDivs = numberOfDivs * numberOfDivs;
-    for (let i = 0; i < numberOfDivs; i++) {
+    let totalNumberOfDivs = numberOfDivs * numberOfDivs;
+    heightOfDiv = (600 / numberOfDivs) - 2;
+    widthOfDiv = (600 / numberOfDivs) - 2;
+
+    let board = document.querySelector('.mainContainer');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
+    for (let i = 0; i < totalNumberOfDivs; i++) {
         const divs = document.createElement('div');
         divs.classList.add('littleDivs');
         mainContainer_div.appendChild(divs);
         divs.style.cssText = `height: ${heightOfDiv}px; width: ${widthOfDiv}px; border-width: 1px; background: white; display: flex; border-color: grey; border-style: solid; flex-wrap: nowrap`;
+        
     }
 }
 
-createDivs(sliderValue);
+createDivs(16);
+
+function changeSize(input) {
+    createDivs(input);
+}
+
+
 
 // function changeColorHover() {
-//     const divs_div = document.querySelector('.littleDivs');
-//     divs_div.addEventListener('mouseenter', () => {
-//         divs_div.style.background = 'black';
-//     });
-// }
+    //     const divs_div = document.querySelector('.littleDivs');
+    //     divs_div.addEventListener('mouseenter', () => {
+        //         divs_div.style.background = 'black';
+        //     });
+        // }
+        
+        // changeColorHover();
+        
 
-// changeColorHover();
 
-
-
-
-// const divs_div = document.getElementsByClassName('littleDivs');
-// divs_div.addEventListener("mouseenter", () => divs_div.style.background = "black");
-
-// divs_div.addEventListener("mouseleave", () => console.log('chau'));
-
-// function mouseOver() {
-//     divs_div.style.cssText = "background-color: black";
-// }
-
-// function mouseOut() {
-//     divs_div.style.cssText = "background: black";
+        
+        // const divs_div = document.getElementsByClassName('littleDivs');
+        // divs_div.addEventListener("mouseenter", () => divs_div.style.background = "black");
+        
+        // divs_div.addEventListener("mouseleave", () => console.log('chau'));
+        
+        // function mouseOver() {
+            //     divs_div.style.cssText = "background-color: black";
+            // }
+            
+            // function mouseOut() {
+                //     divs_div.style.cssText = "background: black";
 // }
