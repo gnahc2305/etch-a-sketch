f93990c799dc55cf44e43ad799d799234887485a
gnahc2305 <achangmurillo@gmail.com>
2022-09-14 19:37:21 -0500

    All squares now perfectly fit inside the main Div

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const mainContainer_div = document.querySelector('.mainContainer');
+let sliderValue = document.getElementById('slider').value;
+
+function calculateSize(sliderValue) {
+    const mainContainerHeight = 600;
+    const mainContainerWidth = 600;
+    heightOfDiv = (mainContainerHeight / sliderValue) - 2;
+    widthOfDiv = (mainContainerWidth / sliderValue) - 2;
+}
+
+calculateSize(sliderValue);
 
 function createDivs(numberOfDivs) {
+    numberOfDivs = numberOfDivs * numberOfDivs;
     for (let i = 0; i < numberOfDivs; i++) {
         const divs = document.createElement('div');
         mainContainer_div.appendChild(divs);
-        divs.style.cssText = 'height: 100px; width: 100px; background: black; display: flex; margin: 10px; flex-wrap: wrap;';
+        divs.style.cssText = `height: ${heightOfDiv}px; width: ${widthOfDiv}px; border-width: 1px; background: white; display: flex; border-color: grey; border-style: solid; flex-wrap: nowrap`;
     }
 }
 
-createDivs(16);
+createDivs(sliderValue);
