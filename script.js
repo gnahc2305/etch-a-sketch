17ed28bdfbe7f9c9ffe23df5ba8114bc3b42c8e0
gnahc2305 <achangmurillo@gmail.com>
2022-09-14 22:06:49 -0500

    Add textbox instead of range slider

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainContainer_div = document.querySelector('.mainContainer');
-let sliderValue = document.getElementById('slider').value;
+let sliderValue = 16 // document.getElementById('slider').value;
+
 
 function calculateSize(sliderValue) {
     const mainContainerHeight = 600;
@@ -14,9 +15,35 @@ function createDivs(numberOfDivs) {
     numberOfDivs = numberOfDivs * numberOfDivs;
     for (let i = 0; i < numberOfDivs; i++) {
         const divs = document.createElement('div');
+        divs.classList.add('littleDivs');
         mainContainer_div.appendChild(divs);
         divs.style.cssText = `height: ${heightOfDiv}px; width: ${widthOfDiv}px; border-width: 1px; background: white; display: flex; border-color: grey; border-style: solid; flex-wrap: nowrap`;
     }
 }
 
 createDivs(sliderValue);
+
+// function changeColorHover() {
+//     const divs_div = document.querySelector('.littleDivs');
+//     divs_div.addEventListener('mouseenter', () => {
+//         divs_div.style.background = 'black';
+//     });
+// }
+
+// changeColorHover();
+
+
+
+
+// const divs_div = document.getElementsByClassName('littleDivs');
+// divs_div.addEventListener("mouseenter", () => divs_div.style.background = "black");
+
+// divs_div.addEventListener("mouseleave", () => console.log('chau'));
+
+// function mouseOver() {
+//     divs_div.style.cssText = "background-color: black";
+// }
+
+// function mouseOut() {
+//     divs_div.style.cssText = "background: black";
+// }
