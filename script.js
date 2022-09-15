c4b69a328b274c6319d91be299379a3fa831f983
gnahc2305 <achangmurillo@gmail.com>
2022-09-15 18:28:13 -0500

    Reset button now works

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const mainContainer_div = document.querySelector('.mainContainer');
 const reset_btn = document.querySelector('.reset');
 
 function createDivs(numberOfDivs) {
-    let totalNumberOfDivs = numberOfDivs * numberOfDivs;
     heightOfDiv = (600 / numberOfDivs) - 2;
     widthOfDiv = (600 / numberOfDivs) - 2;
 
@@ -11,7 +10,7 @@ function createDivs(numberOfDivs) {
     squares.forEach((div) => div.remove());
     
     
-    for (let i = 0; i < totalNumberOfDivs; i++) {
+    for (let i = 0; i < numberOfDivs * numberOfDivs; i++) {
         const divs = document.createElement('div');
         divs.classList.add('littleDivs');
         divs.addEventListener('mouseover', () => {
@@ -20,14 +19,11 @@ function createDivs(numberOfDivs) {
         mainContainer_div.appendChild(divs);
         divs.style.cssText = `height: ${heightOfDiv}px; width: ${widthOfDiv}px; border-width: 1px; background: white; display: flex; border-color: grey; border-style: solid; flex-wrap: nowrap`;
     }
-    // reset_btn.addEventListener('click', () => {
-    //     squares.forEach((div) => div.style.background = 'white');
-    //     console.log('test');
-    // })
-    
 }
 
 createDivs(36);
+
+let currentSize = 36;
 
 function changeSize(input) {
     if (input >= 2 && input <= 100) {
@@ -35,41 +31,9 @@ function changeSize(input) {
     } else {
         console.log('error')
     }
+    currentSize = input;
 }
 
-// reset_btn.addEventListener('click', () => {
-//     let rstDivs = document.querySelectorAll('littleDivs');
-//     rstDivs.style.background = 'red';
-//     console.log(typeof rstDivs);
-// })
-
-function resetBoard() {
-    let rstDivs = document.querySelectorAll('littleDivs');
-    rstDivs.style.background = 'white';
+function resetGame() {
+    createDivs(currentSize);
 }
-
-
-// function changeColorHover() {
-    //     const divs_div = document.querySelector('.littleDivs');
-    //     divs_div.addEventListener('mouseenter', () => {
-        //         divs_div.style.background = 'black';
-        //     });
-        // }
-        
-        // changeColorHover();
-        
-
-
-        
-        // const divs_div = document.getElementsByClassName('littleDivs');
-        // divs_div.addEventListener("mouseenter", () => divs_div.style.background = "black");
-        
-        // divs_div.addEventListener("mouseleave", () => console.log('chau'));
-        
-        // function mouseOver() {
-            //     divs_div.style.cssText = "background-color: black";
-            // }
-            
-            // function mouseOut() {
-                //     divs_div.style.cssText = "background: black";
-// }
